d837769350038c8dcf7ef3edc50e62351330e63e
pnathan <pnathan@vandals.uidaho.edu>
2013-06-19 22:28:40 -0700

    Fix #2. Refactored aggregations/group bys; added SUM.
    Aggregations and group-bys were linked; this patch decouples them,
    fixing bug #2.
    
    Also adding SUM and REDUCE as queries.

--- cl-linq.lisp
+++ cl-linq.lisp
@@ -50,12 +50,8 @@ the top of each iteration."
             (list
              (funcall columns row))))))
 
-(defun group-by-tool (group-by aggregation-function-list results)
-  ;;initial cut: taking the index of the table to group by, along with
-  ;;an aggregation function list.  Each aggregation function will be
-  ;;executed on the entire group-by table, so it should take a 2d list
-  ;;of lists. Since SELECT returns a 2d list of lists at present and
-  ;;this reads SELECT's output, this should not be a problem.
+(defun group-by-tool (group-by results)
+  ;;initial cut: taking the index of the table to group by
 
   ;; WARNING: we are grouping by EQUALP. Probably not ideal.
   (let ((dummy-table (make-hash-table :test #'equalp)))
@@ -68,15 +64,7 @@ the top of each iteration."
                     (setf (gethash row-idx dummy-table)
                           (list row))
                     (push row (gethash row-idx dummy-table))))))
-    (let ((results (alexandria:hash-table-alist dummy-table)))
-      (if aggregation-function-list
-          (loop for group in results
-             collect
-               (cons (car group)
-                     (loop for function in aggregation-function-list
-                        collect
-                          (funcall function (cdr group)))))
-          results))))
+    (alexandria:hash-table-alist dummy-table)))
 
 
 
@@ -85,27 +73,62 @@ the top of each iteration."
                  (predicate nil)
                  (aggregation-functions nil)
                  (group-by nil))
+
   (let ((results)
         (data-length (length data)))
+
     ;; Conditionally select data
     (loop
        for i from 0 below data-length do
        ;; iteraton via elt to support the sequence abstraction
-      (let ((row (elt data i)))
+         (let ((row (elt data i)))
 
-        (if predicate
-             (when (funcall predicate row)
-               (push row results))
-             (push row results))))
+           (if predicate
+               (when (funcall predicate row)
+                 (push row results))
+               (push row results))))
 
+    ;; Select columns
     (let ((selected-results
            (if (or (eq columns t)
                    (eq columns '*))
                results
                (select-columns results columns))))
-      (if group-by
-          (group-by-tool group-by aggregation-functions selected-results)
-          selected-results))))
+
+      ;; Based on the selected columns, determine if we need to GROUP
+      ;; BY.
+      (let ((grouped-results
+             (if group-by
+                 (group-by-tool
+                  group-by selected-results)
+                 selected-results)))
+
+        (if aggregation-functions
+
+            (loop for group in grouped-results
+               collect
+                 (let ((aggregation
+                        ;; Special case it based on whether we get a
+                        ;; list of functions or not.
+                        (if (consp aggregation-functions)
+                            (loop for function in aggregation-functions
+                               collect
+                                 ;; special case it: group-bys have a
+                                 ;; different structure than regular
+                                 ;; tables.
+                                 (if group-by
+                                     (funcall function (cdr group))
+                                     (funcall function group)))
+                            (if group-by
+                                (funcall aggregation-functions (cdr group))
+                                (funcall aggregation-functions group)))))
+
+                   ;; cons in the key to the group
+                   (if group-by
+                       (cons (car group)
+                             aggregation)
+                       aggregation)))
+            grouped-results)))))
 
 (defmacro select-parser (&rest args)
   "SELECT (t | <list of zero-indexed columns>) FROM <data> (WHERE predicate)
@@ -119,7 +142,7 @@ Data is expected to be a 2D loopable list of lists."
         (1+ where-pos)))
 
 
-  (let* ((where-form
+  (let ((where-form
           (list :predicate
                 (awhen (position 'WHERE args)
                   (elt args (1+ it)))))
@@ -152,9 +175,15 @@ Data is expected to be a 2D loopable list of lists."
 ;;(any-parser #'oddp '(1 2 5))
 
 (defmacro contains-parser (val seq)
-  `(any-parser #'(lambda (ele) (eql ele ,val)) ,seq))
+  `(any-parser
+    #'(lambda (ele)
+        (eql ele ,val))
+    ,seq))
 
 ;;(contains-parser 3 '(1 2 3 4))
+
+(defmacro sum-parser (data key)
+  `(reduce #'+ ,data :key ,key))
 
 (defmacro query (operation &rest args)
   (ecase operation
@@ -166,6 +195,10 @@ Data is expected to be a 2D loopable list of lists."
      `(contains-parser ,(first args) ,(second args)))
     (any
      `(any-parser ,(first args) ,(second args)))
+    (sum
+     `(sum-parser ,(first args) ,(second args)))
+    (reduce
+     `(reduce ,(second args) ,(first args) ,(third args)))
     (select
      `(select-parser ,@args))))
 
